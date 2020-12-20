d00fc280c75f3d49fa4dbcebb7029e313ddb10c8
marcalph <marcalph@protonmail.com>
2020-12-20 12:11:01 +0100

    wip nox

--- noxfile.py
+++ noxfile.py
@@ -1,31 +1,47 @@
+#!/usr/bin/env python3
+# coding: utf-8
+##########################################
+# authors                                #
+# marcalph - https://github.com/marcalph #
+##########################################
+"""code automation w/ nox
+"""
 import nox
-import tempfile
 
 locations = "noxfile.py", "src", "tests"
 
 
 @nox.session(python="3.8.6")
 def test(session):
+    """test session"""
     session.run("pytest", "--cov=src", external=True)
 
 
 @nox.session(python="3.8.6")
 def lint(session):
+    """lint session w/ flake8
+    bandit      > code security
+    black       > code format
+    bugbear     > code design
+    docstrings  > code doc
+    """
     args = session.posargs or locations
-    session.install("flake8", "flake8-bandit", "flake8-black", "flake8-bugbear")
+    session.install(
+        "flake8", "flake8-bandit", "flake8-black", "flake8-bugbear", "flake8-docstrings"
+    )
     session.run("flake8", *args)
 
 
 @nox.session(python="3.8.6")
-def black(session):
+def format(session):
+    """code formatting session w/ black"""
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
 
 
 @nox.session(python="3.8.6")
-def safety(session):
-    """ check dependencies for known security vulnerabilities
-    """
+def secure(session):
+    """check dependencies for known vulnerabilities"""
     session.install("safety")
     session.run("safety", "check", "--file=demo/requirements.txt")
